548f03032d74089de847df289263030d082518e2
Divyanshu Mayank <dmayank@adobe.com>
2024-11-25 10:08:47 +0530

    fix for category landing page

--- resources/script/module/product page/filter-checkbox-selector.js
+++ resources/script/module/product page/filter-checkbox-selector.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-restricted-globals
-const URL_PATHNAME = location.pathname.split("/").slice(-1);
+const URL_PATHNAME = location.pathname.split('/').slice(-1)[0];
 
 const pathNameAndClassname = {
   men: 'men',
@@ -20,19 +20,19 @@ const checkElement = (elementClassName) => {
 
 // Loop over the pathNameAndClassname object and assign the classname according to the document path
 const checkPathname = () => {
-  if (URL_PATHNAME === '/men-outerwear.html') {
+  if (URL_PATHNAME === 'men-outerwear' || URL_PATHNAME === 'men-outerwear.html') {
     checkElement(pathNameAndClassname.men);
   }
 
-  if (URL_PATHNAME === '/women-clothing.html') {
+  if (URL_PATHNAME === 'women-clothing' || URL_PATHNAME === 'women-clothing.html') {
     checkElement(pathNameAndClassname.women);
   }
 
-  if (URL_PATHNAME === '/jewelery.html') {
+  if (URL_PATHNAME === 'jewelery' || URL_PATHNAME === 'jewelery.html') {
     checkElement(pathNameAndClassname.jewelery);
   }
 
-  if (URL_PATHNAME === '/electronics.html') {
+  if (URL_PATHNAME === 'electronics' || URL_PATHNAME === 'electronics.html') {
     checkElement(pathNameAndClassname.electronics);
   }
 };
